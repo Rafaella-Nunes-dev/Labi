737700cde665dea430a620e992734a8d5607633c
Rafaella <rafaella11garcia@gmail.com>
2026-03-16 20:33:52 -0300

    Adicionei o método de esconder e aparecer divs e melhorei o css

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
     $("#sobre").click(function(){
         $(this).css("color", "green");
+        $("#equipe-info").hide();
+        $("#sobre-info").show();
+        $("#sobre-info").css("color", "green");
+        $("h1").css("color", "green");
+        $("body").css("background-color", "darkolivegreen")
+    });
+});
+
+$(document).ready(function(){
+    $("#equipe").click(function(){
+        $(this).css("color", "blue");
+        $("#sobre-info").hide();
+        $("#equipe-info").show();
+        $("h1").css("color", "blue");
+        $("h1 border").css("color", "blue");
+        $("#equipe-info").css("color", "lightskyblue");
+        $("body").css("background-color", "rgb(2, 54, 88)")
     });
 });
